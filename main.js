e2b92ca1e7286d6b1b9fc713e1b55683dda51d0b
Kurt Krafft <kkrafft6609@gmail.com>
2020-01-20 15:09:19 -0600

    filter testing

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const takeNumber = function (bandName) {
 
 // const under = takeNumber("Underdogs")
 // console.log(under)  // This should print "2. Underdogs" in the console
+const stageNumber = 1;
 
 const bandsArr = [];
 const storeBand = (band) => {
@@ -19,16 +20,16 @@ const storeBand = (band) => {
         bandBio: document.querySelector('.bandBio').value,
         bandGenre: document.querySelector('#bandGenre').value,
         bandsNumber: bandNumber
-        }
-        bandNumber++;
-        bandsArr.unshift(newBand);
-        console.log(bandsArr)
     }
+    bandNumber++;
+    bandsArr.unshift(newBand);
+    // console.log(bandsArr)
+}
 
 const getNumber = () => {
-   let html = `<div class="urNumba"> <h6>Thanks for submitting ${bandsArr[0].bandName}!</h6><h7>Your number is: ${bandsArr[0].bandsNumber}`
-   const numberDiv = document.querySelector('.urNumber');
-   numberDiv.innerHTML=html;
+    let html = `<div class="urNumba"> <h6>Thanks for submitting ${bandsArr[0].bandName}!</h6><h7>Your number is: ${bandsArr[0].bandsNumber}`
+    const numberDiv = document.querySelector('.urNumber');
+    numberDiv.innerHTML = html;
 }
 
 const formButton = document.getElementById('submitBand');
@@ -36,3 +37,26 @@ formButton.addEventListener('click', storeBand);
 
 const numButton = document.getElementById('bandNum');
 numButton.addEventListener('click', getNumber);
+
+const checkBandNumber = () => {
+    let bandSearch = document.querySelector('.numLookup').value;
+    let userBand = bandsArr.filter(function (item) {
+        return item.bandName === bandSearch;
+    });
+    alert(`Hello ${userBand[0].bandName} your number is ${userBand[0].bandsNumber}.`);
+}
+const searchButton = document.querySelector('.btnSearch');
+searchButton.addEventListener('click', checkBandNumber);
+
+
+// const checkBandNumber = () => {
+//     let bandSearch = document.querySelector('.numLookup').value;
+//     for (let i=0; i<bandsArr.length; i++){
+//         if(bandSearch === bandsArr[i].bandName){
+//             alert(`Awesome ${bandsArr[i].bandName}, your number is ${bandsArr[i].bandsNumber} and the band that is on stage is number ${stageNumber}.`)
+//         } else {
+//             // alert("Sorry we couldn't find your band, are you sure you spelled it correctly?");
+//             continue
+//         }
+//     }
+// }
